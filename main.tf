d2a8a6ceec0c13b3db61b3ab2e3107e99e5ca118
Umar Ali <umarali@Umars-MacBook-Pro.local>
2024-11-20 14:48:01 +0530

    Added rg id

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 data "ibm_resource_instance" "testacc_ds_resource_instance" {
   name              = "cos-instance"
   location          = "global"
-  resource_group_id = "Default"
+  resource_group_id = "b180729994c44e88b682384025cfd7f8"
 }
