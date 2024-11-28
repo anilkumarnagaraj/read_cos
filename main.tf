65cc2c4ca693ee755daf72969ae00d39753b3552
umarali-nagoor <55839118+umarali-nagoor@users.noreply.github.com>
2024-11-28 14:55:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,10 @@ data "ibm_resource_instance" "testacc_ds_resource_instance" {
   location          = "global"
   resource_group_id = "b180729994c44e88b682384025cfd7f8"
 }
+
+data "ibm_cos_bucket" "standard-ams03" {
+  bucket_name          = "sch-cos-cbr"
+  resource_instance_id = data.ibm_resource_instance.testacc_ds_resource_instance.id
+  bucket_type          = "single_site_location"
+  bucket_region        = "us-geo"
+}
