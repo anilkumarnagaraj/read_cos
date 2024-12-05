512cf5f93829ad6ec7c67176e2c35f2b1496f020
anilkumarnagaraj <anilkumar@in.ibm.com>
2024-12-05 08:40:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,4 +9,5 @@ data "ibm_cos_bucket" "standard-ams03" {
   resource_instance_id = data.ibm_resource_instance.testacc_ds_resource_instance.id
   bucket_type          = "cross_region_location"
   bucket_region        = "us"
+  endpoint_type        = "private"
 }
