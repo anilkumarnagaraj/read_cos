66dc2fe2db88d02dca39b15f06f95c8723356e9a
anilkumarnagaraj <anilkumar@in.ibm.com>
2024-12-05 08:45:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+/*
 data "ibm_resource_instance" "testacc_ds_resource_instance" {
   name              = "cos-cbr-test"
   location          = "global"
@@ -9,5 +10,21 @@ data "ibm_cos_bucket" "standard-ams03" {
   resource_instance_id = data.ibm_resource_instance.testacc_ds_resource_instance.id
   bucket_type          = "cross_region_location"
   bucket_region        = "us"
+  endpoint_type        = "private"
+}
+
+*/
+
+data "ibm_resource_instance" "testacc_ds_resource_instance" {
+  name              = "cos-instance"
+  location          = "global"
+  resource_group_id = "b180729994c44e88b682384025cfd7f8"
+}
+
+data "ibm_cos_bucket" "standard-ams03" {
+  bucket_name          = "sch-cos-cbr"
+  resource_instance_id = data.ibm_resource_instance.testacc_ds_resource_instance.id
+  bucket_type          = "cross_region_location"
+  bucket_region        = "us-geo"
   endpoint_type        = "private"
 }
